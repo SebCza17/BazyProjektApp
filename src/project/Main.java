5eba14498ce2d4ef13eaf12240ded90bc4ebf101
Rajvi <urbanskib1@gmail.com>
2017-11-28 22:25:39 +0100

    test

--- src/project/Main.java
+++ src/project/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/MainScene.fxml"));
 
         Button btn1 = new Button("Test");
 
