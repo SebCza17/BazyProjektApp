08faa22ed8808855ac24f01f1ff02e4b5e0a1730
Rajvi <urbanskib1@gmail.com>
2017-11-28 22:48:46 +0100

    test

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,20 +4,53 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private Stage primaryStage;
+    private BorderPane MainScene;
+    private GridPane LoginOrRegister;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/MainScene.fxml"));
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("BazyProjektApp");
 
-        Button btn1 = new Button("Test");
+        initMainScene();
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        initLoginOrRegister();
+    }
+
+    public void initMainScene() {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("FXML/MainScene.fxml"));
+            MainScene = loader.load();
+
+            Scene scene = new Scene(MainScene);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void initLoginOrRegister() {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("FXML/LoginOrRegister.fxml"));
+            LoginOrRegister = loader.load();
+
+            MainScene.setCenter(LoginOrRegister);
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
